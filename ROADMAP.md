99a01d2b4f598360aa9a26eb17b415b45bb57a28
WazedKhan <wazedkhan119399@gmail.com>
2024-11-20 08:04:40 +0600

    type-enforcer-0.1.0

---
+++ ROADMAP.md
@@ -0,0 +1,5 @@
+Next Step:
+
+* Adding PyPI support.
+* Writing more extensive tests.
+* Extending support for complex types like `List[int]`, `Union[str, None]`
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="enforcer",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[],
+    python_requires=">=3.7",
+    description="A lightweight decorator for enforcing type hints at runtime.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/WazedKhan/enforcer",
+    author="Wajed Khan",
+    author_email="wajed.abdul.khan@gamil.com",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
